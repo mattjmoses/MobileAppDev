6df772e2338c9881d1cf755fdfdd1901740d9388
Matt <mattjmoses@gmail.com>
2017-09-24 16:12:24 -0300

    Assignment 01
    Everything's more or less working. Now it's time to code up a goddamn calculator.

---
+++ Assignments/Assignment_01/app/src/main/java/com/example/matt/assignment_01/MathHappener.java
@@ -0,0 +1,25 @@
+package com.example.matt.assignment_01;
+
+/**
+ * Created by Matt on 2017-09-24.
+ */
+
+//This class is where the magic of mathematics happens. OH MY GOD
+
+public class MathHappener {
+
+
+    //This method takes in the values from the Calculator class and makes math happen to them.
+    //Hoot hoot.
+    public double makeMathHappen(String value01, String operator, String value02)
+    {
+        //First thing. Convert both values to doubles so they can be math'd
+
+        return 0;
+    }
+
+
+
+
+
+}//End Class
--- Assignments/Assignment_01/app/src/main/java/com/example/matt/assignment_01/mathHappener.java
+++
@@ -1,8 +0,0 @@
-package com.example.matt.assignment_01;
-
-/**
- * Created by Matt on 2017-09-24.
- */
-
-public class mathHappener {
-}
