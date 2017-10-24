433707240af2e5187ad05dc0030fafb1b6f224bb
Matt <mattjmoses@gmail.com>
2017-10-23 22:01:07 -0300

    Bloody Android Studio

---
+++ Assignments/Assignment_02/app/src/main/java/com/mattiscool/matt/assignment_02/QuizLogic.java
@@ -0,0 +1,19 @@
+package com.mattiscool.matt.assignment_02;
+
+/**
+ * Created by Matt on 2017-10-23.
+ */
+
+public class QuizLogic
+{
+
+    private String[] questions = {"What's 1+1","What's 2+2","What's 3+3","What's 4+4"};
+    private String[][] choices = {
+            {"Banana","2","Idiot"},
+            {"4","Jump","Wrong"},
+            {"Mando","6","OuterSpacePotatoMan"},
+            {"Nun","Bisquick","8"}
+    };
+    private String[] answers = {"2","4","6","8"};
+
+}//End Class
