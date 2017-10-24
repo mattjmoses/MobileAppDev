13be7360af40b8a5a1b3a62481661fefd39c1397
Matt <mattjmoses@gmail.com>
2017-10-23 22:22:28 -0300

    Buttoning Up Buttons

--- Assignments/Assignment_02/app/src/main/java/com/mattiscool/matt/assignment_02/MainActivity.java
+++ Assignments/Assignment_02/app/src/main/java/com/mattiscool/matt/assignment_02/MainActivity.java
@@ -2,12 +2,64 @@ package com.mattiscool.matt.assignment_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Instantiating the quiz logic class.
+    private QuizLogic quizLogic = new QuizLogic();
+
+    //The UI elements of the app
+    private TextView scoreView;
+    private TextView questionPanel;
+    private Button button01;
+    private Button button02;
+    private Button button03;
+
+
+    private String question;
+    private String answer;
+    private int questionNumber = 0;
+    private int score = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Hooking everything up.
+        scoreView = (TextView)findViewById(R.id.txtScoreCount);
+        questionPanel = (TextView)findViewById(R.id.txtQuestionBox);
+        button01 = (Button)findViewById(R.id.btn01);
+        button02 = (Button)findViewById(R.id.btn02);
+        button03 = (Button)findViewById(R.id.btn03);
+
+        //Button01 Listener
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //Button02 Listener
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //Button03 Listener
+        button03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }//End onCreate
 }
--- Assignments/Assignment_02/app/src/main/java/com/mattiscool/matt/assignment_02/QuizLogic.java
+++ Assignments/Assignment_02/app/src/main/java/com/mattiscool/matt/assignment_02/QuizLogic.java
@@ -6,14 +6,47 @@ package com.mattiscool.matt.assignment_02;
 
 public class QuizLogic
 {
-
-    private String[] questions = {"What's 1+1","What's 2+2","What's 3+3","What's 4+4"};
-    private String[][] choices = {
+    //This is all prototyping. Next step is to use two arraylists with hashing. Then after THAT
+    //Two arraylists built from a text file.
+    private String[] questionList = {"What's 1+1","What's 2+2","What's 3+3","What's 4+4"};
+    private String[][] choiceList = {
             {"Banana","2","Idiot"},
             {"4","Jump","Wrong"},
             {"Mando","6","OuterSpacePotatoMan"},
             {"Nun","Bisquick","8"}
     };
-    private String[] answers = {"2","4","6","8"};
+    private String[] answerList = {"2","4","6","8"};
+
+
+    public String getQuestion(int q)
+    {
+        String question = questionList[q];
+        return question;
+    }
+
+    public String getChoice01(int b)
+    {
+     String choice0 = choiceList[b][0];
+        return choice0;
+    }
+
+    public String getChoice02(int b)
+    {
+        String choice1 = choiceList[b][1];
+        return choice1;
+    }
+
+    public String getChoice03(int b)
+    {
+        String choice2 = choiceList[b][2];
+        return choice2;
+    }
+
+    public String getCorrectAnswer(int a)
+    {
+        String correct = answerList[a];
+        return correct;
+    }
+
 
 }//End Class
