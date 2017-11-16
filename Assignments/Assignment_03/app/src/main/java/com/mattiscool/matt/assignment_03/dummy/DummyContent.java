c65192a1838a4bd039254bb87d21d14b64be8a5a
Matt <mattjmoses@gmail.com>
2017-11-16 16:22:42 -0400

    Finally got the image to duisplay on the fragment. THAT'S ONE part kinda done.

---
+++ Assignments/Assignment_03/app/src/main/java/com/mattiscool/matt/assignment_03/dummy/DummyContent.java
@@ -0,0 +1,75 @@
+package com.mattiscool.matt.assignment_03.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+//        // Add some sample items.
+//        for (int i = 1; i <= COUNT; i++) {
+//            addItem(createDummyItem(i));
+        addItem(new DummyItem("1","Image_01","http://www.imgur.com"));
+        addItem(new DummyItem("2","Image_02","http://www.somethingawful.com"));
+        addItem(new DummyItem("3","Image_03","http://www.google.ca"));
+        }
+
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static DummyItem createDummyItem(int position) {
+        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public final String id;
+        public final String imageName;
+        public final String url;
+
+        public DummyItem(String id, String imageName, String url) {
+            this.id = id;
+            this.imageName = imageName;
+            this.url = url;
+        }
+
+        @Override
+        public String toString() {
+            return imageName;
+        }
+    }
+}
